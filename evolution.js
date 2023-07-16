b9195dfe43d6e0cbe2184eb166c308dbfb28823e
Rahul Hari <torahulhari@gmail.com>
2023-07-16 20:52:19 +0530

    evolution-edit

---
+++ evolution.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var scrollButton = document.getElementById('scrollButton');
+    var targetDiv = document.getElementById('targetDiv');
+  
+    scrollButton.addEventListener('click', function() {
+      targetDiv.scrollIntoView({ behavior: 'smooth' });
+    });
+  };
+  
