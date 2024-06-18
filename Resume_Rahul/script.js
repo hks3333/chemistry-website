0a24b9f9b282f4620853a31d154daff41df131c1
hks3333 <harikris177@gmail.com>
2024-06-18 23:45:20 +0530

    polished

--- Resume_Rahul/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("arrow").addEventListener("click", function() {
-    // Fade in effect
-    document.getElementById("header").style.opacity = 1;
-
-    // Redirect to Page 2 after the fade-in effect is complete
-    setTimeout(function() {
-        window.location.href = "index.html";
-    }, 1000); // 1000ms = 1 second, adjust as needed
-});
